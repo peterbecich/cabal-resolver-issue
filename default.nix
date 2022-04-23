3eca518459dd036fb28bf10a11057e480be15ba1
Peter Becich <peterbecich@gmail.com>
2022-04-22 23:54:46 -0700

    test

--- default.nix
+++ default.nix
@@ -1,25 +1,16 @@
-{ #compiler ? "ghc8107",
-  # ghcjsVersion ? "8.8.4",
-  #ghcVersion ? "8.10.7",
-  withCoverage ? false,
-  doCheck ? false,
-  doCoverage ? false,
-  doHaddock ? false,
-}:
-  let
-    sources = import ./nix/sources.nix {};
+let
+  sources = import ./nix/sources.nix {};
 
-    haskellNix = import sources.haskellNix {};
-    pkgs = import
+  haskellNix = import sources.haskellNix {};
+  pkgs = import
 
-      haskellNix.sources.nixpkgs-2111
+    haskellNix.sources.nixpkgs-2111
 
-      haskellNix.nixpkgsArgs;
-  in pkgs.haskell-nix.project {
-    src = pkgs.haskell-nix.haskellLib.cleanGit {
-      name = "cabal-resolver-issue";
-      src = ./.;
-      # exactDeps = true;
-    };
-    compiler-nix-name = "ghc8107";
-  }
+    haskellNix.nixpkgsArgs;
+in pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "cabal-resolver-issue";
+    src = ./.;
+  };
+  compiler-nix-name = "ghc8107";
+}
